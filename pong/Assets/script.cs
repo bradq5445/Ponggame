95bdf00c5923c48bd8ff487a7b86ea44ae87aea5
brad quijano <bradquijano@gmail.com>
2024-04-19 19:34:02 -0400

    added ai movement

--- pong/Assets/script.cs
+++ pong/Assets/script.cs
@@ -20,3 +20,21 @@ public class Playermovement : MonoBehaviour
         }
     }
 }
+
+public class Aimovement: MonoBehaviour
+    //Vector3 for going up and down
+Vector3 speedUp = Vector3.up;
+Vector3 speedDown = Vector3.down;
+float speed = 0.1f;
+// Update is called once per frame
+void FixedUpdate()
+{
+    //Move paddle up
+    transform.position += speedUp * speed;
+}
+
+//Swap direction when wall is hit
+void OnCollisionEnter2D(Collision2D collision)
+{
+    speed = speed * -1;
+}
